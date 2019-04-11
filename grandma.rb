10373d96ab77e40b47bf1e2a4a867317356605ab
Conrad Casper <conradcasper@icloud.com>
2019-04-11 01:47:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
-    puts ""
+    puts "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
